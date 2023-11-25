925c32c3ec55306f0e3126025c926c1c8dc92c0e
ssb0819 <ssb0819@naver.com>
2023-11-25 16:34:21 +0900

    printf 진행중

--- ft_printf/test/test_etc.c
+++ ft_printf/test/test_etc.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <float.h>
+#include <math.h>
+#include <limits.h>
 
-int	main(void)
+void	get_bufsize()
 {
 	printf("%d\n", BUFSIZ);
 	printf("%lu\n", sizeof(char *));
 	printf("%x\n", 15);
+}
+
+void	get_min_max()
+{
+	printf("long min: %ld\n", LONG_MIN);
+	printf("long max: %ld\n", LONG_MAX);
+	printf("long long min: %lld\n", LLONG_MIN);
+	printf("long long max: %lld\n", LLONG_MAX);
+	printf("float min: %e\n", FLT_MIN);
+	printf("float max: %e\n", FLT_MAX);
+	printf("double min: %e\n", DBL_MIN);
+	printf("double max: %e\n", DBL_MAX);
+	printf("long double min: %Le\n", LDBL_MIN);
+	printf("long double max: %Le\n", LDBL_MAX);
+}
+
+int	main(void)
+{
+	
 }
